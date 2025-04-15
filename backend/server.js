d42696930102f82bd119a632011d24d24caf02b0
Vijay-2899 <64734271+Vijay-2899@users.noreply.github.com>
2025-04-15 19:52:32 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Middleware
+// Middlewaree
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname)); // Serve your HTML/CSS/JS files
