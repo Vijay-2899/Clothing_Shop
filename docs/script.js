63b29e8cac5d884e21500329bfaedcc869b43216
Vijay-2899 <64734271+Vijay-2899@users.noreply.github.com>
2025-04-16 19:54:27 +0000

    test_script

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const backend = "https://verbose-cod-46jw9vq5qv63x6-4000.app.github.dev/api";
+const backend = "https://clever-raindrop-5b77c9.netlify.app/";
 
 // ===== SIGNUP =====
 const signupForm = document.getElementById('signup-form');
